e69e5563f50783731af2c824620eeb170366395f
Kushina <superaoba0602@gmail.com>
2024-10-15 11:41:21 +0900

    モデル定義の作成

--- Assignment/models.py
+++ Assignment/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from lecture.models import Course
 
-# Create your models here.
+
+class Assignment(models.Model):
+    assignment_id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    deadline = models.DateTimeField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from assignment.models import Assignment
+from lecture.models import Course
 
-# Create your models here.
+
+class CustomUser(AbstractUser):
+    icon = models.ImageField(upload_to='icon/', null=True, blank=True)
+    courses = models.ManyToManyField(Course, related_name='students', blank=True)
+
+    def __str__(self):
+        return self.username
+
+class User_Assignment(models.model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    is_finished = models.BooleanField(default=False)
