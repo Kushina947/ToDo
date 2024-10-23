d66f3d0b8d043f8b1807f48983e7f4101e12a02f
Kushina <superaoba0602@gmail.com>
2024-10-24 00:53:24 +0900

    sign_upの作成

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,6 @@ from django.views.generic.base import TemplateView, RedirectView
 class LoginView(TemplateView):
     template_name = "registration/login.html"
 
-
 class SignUpView(TemplateView):
     template_name = "registration/sign_up.html"
 
