7bbee0bb97c01a3a243c0096fbc81b4f0b6b117b
Kushina <superaoba0602@gmail.com>
2024-10-24 00:46:13 +0900

    base/views.pyでトップページでユーザー情報を表示できるように更新

--- base/views.py
+++ base/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, RedirectView
+from accounts.models import CustomUser, User_Assignment
+from assignment.models import Assignment
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-class HomeView(TemplateView):
+class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "base/home.html"
 
     def get_context_data(self, **kwargs):
         ctx =  super().get_context_data(**kwargs)
-        class Task:
-            def __init__(self, title, description, due_date=None):
-                self.title = title
-                self.description = description
-                self.due_date = due_date
-        task = Task('Home', 'This is the home page.', '2021-01-01')
-        ctx['task'] = task
+        ctx['user_assignments'] = User_Assignment.objects.filter(user=self.request.user)
+        ctx['courses'] = CustomUser.objects.get(id=self.request.user.id).courses.all()
         ctx['user'] = self.request.user
-
         return ctx
--- conf/urls.py
+++ conf/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 import base
 import assignment
 import accounts
@@ -31,3 +33,6 @@ urlpatterns = [
     path('lecture/', include('lecture.urls')),
     path('thread/', include('thread.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
