f94737960c8be6b971eeb914433a3f68822dfe3c
坂井颯真 <tokotoko.sakai@gmail.com>
2024-11-06 16:28:02 +0900

    講義検索機能（途中まで）

--- lecture/forms.py
+++ lecture/forms.py
@@ -1,19 +1,18 @@
 from django import forms
 from .models import Course
-
-class CourseForm(forms.ModelForm):
-    class Meta:
-        model = Course
-        fields = '__all__' 
-    term = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    code = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    teacher = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
-    lecture_schedule = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+from accounts.models import CustomUser
 
 
-    def clean_code(self):
-        code = self.cleaned_data['code']
-        if len(code) > 10:  #
-            raise forms.ValidationError("コードは10文字以内で入力してください。")
-        return code
+class CourseAddForm(forms.Form):
+    search_code = forms.CharField(label='開講番号', max_length=10, required=False)
+    courses = forms.ModelMultipleChoiceField(
+        queryset=Course.objects.none(),
+        widget=forms.CheckboxSelectMultiple,
+        required=False
+    )
+
+    def search_courses(self, search_code):
+        if search_code:
+            self.fields['courses'].queryset = Course.objects.filter(code=search_code)
+        else:
+            self.fields['courses'].queryset = Course.objects.none()
--- lecture/urls.py
+++ lecture/urls.py
@@ -3,5 +3,7 @@ from .views import *
 
 urlpatterns = [
     path('', LectureView.as_view(), name='lecture_list'),
+    path('add', Lecture_addView.as_view(), name='add'),
     path('register/', Lecture_addView.as_view(), name='lecture_add'),
+    path('get_course_details/', get_course_details.as_view(), name='get_course_details'),
 ]
