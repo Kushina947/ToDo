8b05544f1d0aa7aeebc6195a32578a85b2cf85a2
Kushina <superaoba0602@gmail.com>
2024-11-06 16:18:06 +0900

    login signinを実装

--- accounts/form.py
+++ accounts/form.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
 
 class CustomAuthenticationForm(AuthenticationForm):
      def __init__(self, *args, **kwargs):
          kwargs.setdefault('label_suffix', '')
          super().__init__(*args, **kwargs)
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,11 @@
-from .form import CustomAuthenticationForm
+from .form import CustomAuthenticationForm, CustomUserCreationForm
 from django.shortcuts import render
-from django.views.generic.base import TemplateView, RedirectView
+from django.views.generic import TemplateView, RedirectView, FormView
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login
 
 class CustomLoginView(LoginView):
     template_name = "registration/login.html"
@@ -13,8 +16,33 @@ class CustomLoginView(LoginView):
             return redirect('home')
         return super().dispatch(request, *args, **kwargs)
 
-class SignUpView(TemplateView):
+class SignUpView(FormView):
     template_name = "registration/sign_up.html"
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    def form_valid(self, form):
+        print(self.request.POST['next'])
+        if self.request.POST['next'] == 'back':
+            return render(self.request, 'registration/create.html', {'form': form})
+        elif self.request.POST['next'] == 'confirm':
+            return render(self.request, 'registration/sign_up_confirm.html', {'form': form})
+        elif self.request.POST['next'] == 'register':
+            form.save()
+            # 認証
+            user = authenticate(
+                username=form.cleaned_data['username'],
+                password=form.cleaned_data['password1'],
+            )
+            # ログイン
+            login(self.request, user)
+            return super().form_valid(form)
+        else:
+            return redirect(reverse_lazy('signup'))
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home')
+        return super().dispatch(request, *args, **kwargs)
 
 class CustomLogoutView(LogoutView):
     template_name = "registration/logged_out.html"
