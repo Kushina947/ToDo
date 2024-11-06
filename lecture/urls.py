0094285f1e307e53782a3131f36ebd5f014e93c0
Kushina <superaoba0602@gmail.com>
2024-11-06 17:29:01 +0900

    a

--- lecture/urls.py
+++ lecture/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', LectureView.as_view(), name='lecture_list'),
     path('add', Lecture_addView.as_view(), name='add'),
     path('register/', Lecture_addView.as_view(), name='lecture_add'),
-    path('get_course_details/', get_course_details.as_view(), name='get_course_details'),
 ]
