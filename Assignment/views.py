428481a669930828563055eca8473f0f034efb91
坂井颯真 <tokotoko.sakai@gmail.com>
2024-11-14 11:48:40 +0900

    課題関係

--- Assignment/views.py
+++ Assignment/views.py
@@ -13,6 +13,13 @@ class AssignmentCreateView(LoginRequiredMixin, CreateView):
     form_class = AssignmentForm
     template_name = 'assignment/assignment_create.html'
 
+    def get_initial(self):
+        initial = super().get_initial()
+        code = self.kwargs.get('code')
+        course = get_object_or_404(Course, code=code)
+        initial['course'] = course
+        return initial
+
     def get_form_kwargs(self):
         kwargs = super().get_form_kwargs()
         kwargs['initial'] = {'course': get_object_or_404(Course, code=self.kwargs.get('code'))}
@@ -22,7 +29,6 @@ class AssignmentCreateView(LoginRequiredMixin, CreateView):
         context = super().get_context_data(**kwargs)
         context['course'] = get_object_or_404(Course, code=self.kwargs.get('code'))
         return context
-
 
     def form_valid(self, form):
         form.instance.created_by = self.request.user
@@ -34,18 +40,11 @@ class AssignmentCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy('lecture:lecture', kwargs={'pk': self.kwargs.get('code')})
 
-    def get_initial(self):
-        initial = super().get_initial()
-        code = self.kwargs.get('code')
-        course = get_object_or_404(Course, code=code)
-        initial['course'] = course
-        return initial
-
 
 class AssignmentUpdateView(LoginRequiredMixin, UpdateView):
     model = Assignment
     form_class = AssignmentForm
-    template_name = 'assignment/assignment_form.html'
+    template_name = 'assignment/assignment_create.html'
 
 
     def get_success_url(self):
@@ -54,6 +53,8 @@ class AssignmentUpdateView(LoginRequiredMixin, UpdateView):
 
 class AssignmentDeleteView(LoginRequiredMixin, DeleteView):
     model = Assignment
+    template_name = 'assignment/assignment_delete.html'
+    success_url = reverse_lazy('home')
 
 
     def get_success_url(self):
@@ -64,3 +65,4 @@ class AssignmentDetailView(LoginRequiredMixin, DetailView):
     model = Assignment
     template_name = 'assignment/assignment_detail.html'
     context_object_name = 'assignment'
+
